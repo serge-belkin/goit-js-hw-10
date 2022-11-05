dc264b8d75da93c28608e75cd457995b70295100
serge-belkin <cer_fi@yahoo.com>
2022-11-05 10:34:47 +0200

    fix fetch function

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -2,7 +2,7 @@ export const fetchCountries = name => {
   return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name.official,capital,population,flags.svg,languages`)
     .then(response => {
       if (!response.ok) {
-        if response.status === 404 {
+        if (response.status === 404) {
           return [];
         }
         throw new Error(response.status);
