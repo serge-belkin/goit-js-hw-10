a5184b8070f4d8cc8f1883938d09c937cc5026ce
serge-belkin <cer_fi@yahoo.com>
2022-11-05 12:56:00 +0200

    add render func

--- src/index.js
+++ src/index.js
@@ -4,3 +4,21 @@ import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
+
+let getEl = selector => document.querySelector(selector);
+
+const input = getEl('#search-box');
+const countryList = getEl('.country-list');
+const countryInfo = getEl('.country-info');
+
+function renderCountryList(countries) {
+  const markup = countries.map(country => {
+    return `<li>
+    <img src="#{country.flags.svg}" alt="Flag of ${country.name.official}" 
+    width="40" height="30">
+    <p>${country.name.official}</p></li>`;
+  })
+    .join('');
+  countryList.innerHTML = markup;
+}
+
