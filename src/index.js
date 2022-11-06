1083c65b27bcf8b15ed59154a9336342a4551fcd
serge-belkin <cer_fi@yahoo.com>
2022-11-06 13:16:10 +0200

    add country search

--- src/index.js
+++ src/index.js
@@ -3,22 +3,70 @@ import { fetchCountries } from './js/fetchCountries';
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
 
-const DEBOUNCE_DELAY = 300;
-
 let getEl = selector => document.querySelector(selector);
 
 const input = getEl('#search-box');
 const countryList = getEl('.country-list');
 const countryInfo = getEl('.country-info');
 
+const DEBOUNCE_DELAY = 300;
+
+input.addEventListener(
+  'input',
+  debounce(e => {
+      const trimmedValue = input.value.trim();
+         cleanHtml();   
+    if (trimmedValue !== '') {
+        fetchCountries(trimmedValue).then(foundData => {      
+
+        if (foundData.length > 10) {
+          Notiflix.Notify.info(
+            'Too many matches found. Please enter a more specific name.'
+          );
+        } else if (foundData.length === 0) {
+          Notiflix.Notify.failure('Oops, there is no country with that name');
+        } else if (foundData.length >= 2 && foundData.length <= 10) {
+          renderCountryList(foundData);
+        } else if (foundData.length === 1) {
+          renderOneCountry(foundData);
+        }
+      });
+    }
+  }, DEBOUNCE_DELAY)
+);
+
 function renderCountryList(countries) {
-  const markup = countries.map(country => {
-    return `<li>
-    <img src="#{country.flags.svg}" alt="Flag of ${country.name.official}" 
-    width="40" height="30">
-    <p>${country.name.official}</p></li>`;
-  })
+  const markup = countries
+    .map(country => {
+      return `<li>
+      <img src="${country.flags.svg}" alt="Flag of ${
+        country.name.official
+      }" width="30" hight="20">
+        <b>${country.name.official}</b>
+                </li>`;
+    })
     .join('');
   countryList.innerHTML = markup;
 }
+    
+function renderOneCountry(countries) {
+      const markup = countries
+        .map(country => {
+          return `<li>
+      <img src="${country.flags.svg}" alt="Flag of ${
+            country.name.official
+          }" width="30" hight="20">
+            <b>${country.name.official}</b>
+            <p><b>Capital</b>: ${country.capital}</p>
+            <p><b>Population</b>: ${country.population}</p>
+            <p><b>Languages</b>: ${Object.values(country.languages)} </p>
+                </li>`;
+        })
+        .join('');
+      countryList.innerHTML = markup;
+}
 
+function cleanHtml() {
+  countryList.innerHTML = '';
+  countryInfo.innerHTML = '';
+}
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,5 +1,5 @@
 export const fetchCountries = name => {
-  return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name.official,capital,population,flags.svg,languages`)
+  return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
     .then(response => {
       if (!response.ok) {
         if (response.status === 404) {
